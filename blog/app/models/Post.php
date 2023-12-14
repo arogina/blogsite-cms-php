1e3727b1e292678a340605c286a7cb60dfa344a1
Alan Rogina <rogina.alan@gmail.com>
2023-12-14 21:18:55 +0100

    Added User model

--- blog/app/models/Post.php
+++ blog/app/models/Post.php
@@ -1,7 +1,30 @@
 <?php
     class Post {
-        public $id;
-        public $title;
-        public $content;
-        public $author;
+        private int $id;
+        private string $title;
+        private string $content;
+        private string $author;
+
+        function __construct(int $id, string $title, string $content, string $author) {
+            $this->id = $id;
+            $this->title = $title;
+            $this->content = $content;
+            $this->author = $author;
+        }
+
+        public function getId() {
+            return $this->id;
+        }
+        
+        public function getTitle() {
+            return $this->title;
+        }
+        
+        public function getContent() {
+            return $this->content;
+        }
+
+        public function getAuthor() {
+            return $this->author;
+        }
     }
---
+++ blog/app/models/User.php
@@ -0,0 +1,14 @@
+<?php
+    class User {
+        private int $id;
+        private string $username;
+        private string $email;
+        private string $password;
+
+        function __construct(int $id, string $username, string $email, string $password) {
+            $this->id = $id;
+            $this->username = $username;
+            $this->email = $email;
+            $this->password = $password;
+        }
+    }
